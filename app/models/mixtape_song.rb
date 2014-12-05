bbf70a532d0d5dc22ccc5ad4d3b6b3a6297274f2
Joel Hampton <joelhampton@gmail.com>
2014-12-05 12:17:55 +1100

    Installed SoundCloud gem and added column to model

--- app/models/mixtape_song.rb
+++ app/models/mixtape_song.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: mixtape_songs
+#
+#  id         :integer          not null, primary key
+#  song_id    :integer
+#  mixtape_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class MixtapeSong < ActiveRecord::Base
 
   belongs_to :mixtape
